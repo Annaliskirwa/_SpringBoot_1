1d9fa7bb44fb29fd512e1ccceebf32b604e0b9be
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-03 00:33:08 +0300

    rest controller for getting all employees

--- src/main/java/com/example/EmployeeManagement/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeeManagement/controller/EmployeeController.java
@@ -1,4 +1,26 @@
 package com.example.EmployeeManagement.controller;
 
+import com.example.EmployeeManagement.model.Employee;
+import com.example.EmployeeManagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1")
 public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
 }
