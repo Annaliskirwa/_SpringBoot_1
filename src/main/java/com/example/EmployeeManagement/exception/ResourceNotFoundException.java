a92705232f81feb8174d58142888a7a44a64f871
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-03 01:00:29 +0300

    handling exception thrown from resource

--- src/main/java/com/example/EmployeeManagement/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/EmployeeManagement/exception/ResourceNotFoundException.java
@@ -1,5 +1,7 @@
 package com.example.EmployeeManagement.exception;
 
 public class ResourceNotFoundException extends Exception {
-
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
