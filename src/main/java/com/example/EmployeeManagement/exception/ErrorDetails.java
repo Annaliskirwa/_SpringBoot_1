15be0570f1b9f722d90b2bc76f8319c00a5fbf2a
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-03 13:00:29 +0300

    creates a java model class for error details

--- src/main/java/com/example/EmployeeManagement/exception/ErrorDetails.java
+++ src/main/java/com/example/EmployeeManagement/exception/ErrorDetails.java
@@ -1,4 +1,8 @@
 package com.example.EmployeeManagement.exception;
 
+import java.util.Date;
 public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
 }
