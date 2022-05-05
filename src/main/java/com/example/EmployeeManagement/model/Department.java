af8d7e92035d0c46d39a090f8cdc2e509e3bc9a1
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-05 10:11:15 +0300

    Created the no args constructor

--- src/main/java/com/example/EmployeeManagement/model/Department.java
+++ src/main/java/com/example/EmployeeManagement/model/Department.java
@@ -2,8 +2,14 @@ package com.example.EmployeeManagement.model;
 
 import javax.persistence.Entity;
 
+@Entity
+@Table(name = "department")
 public class Department {
     private int departmentId;
     private String departmentName;
     private int departmentCountryId;
+
+    public Department(){
+
+    }
 }
