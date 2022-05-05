8b8a2ee0d0cfe5b1816590257f55cc58c317bb31
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-05 23:50:43 +0300

    delete a department by id

--- src/main/java/com/example/EmployeeManagement/controller/DepartmentController.java
+++ src/main/java/com/example/EmployeeManagement/controller/DepartmentController.java
@@ -10,7 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -34,6 +36,17 @@ public class DepartmentController {
         Department department = departmentRepository.findById(departmentId)
                 .orElseThrow(()-> new ResourceNotFoundException("The department with that id not found:" + departmentId));
         return ResponseEntity.ok().body(department);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public Map<String, Boolean> deleteDepartmentByIdw(@PathVariable(value = "id") Long departmentId)
+        throws ResourceNotFoundException{
+        Department department = departmentRepository.findById(departmentId)
+                .orElseThrow(()-> new ResourceNotFoundException("The department with that id not found:" + departmentId));
+        departmentRepository.delete(department);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("The department was deleted", Boolean.TRUE);
+                return response;
     }
 
 
