c6ad81c4f86a149f25c23ef4131b8431ec0fe012
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-05 23:44:17 +0300

    getting a department by id

--- src/main/java/com/example/EmployeeManagement/controller/DepartmentController.java
+++ src/main/java/com/example/EmployeeManagement/controller/DepartmentController.java
@@ -1,10 +1,12 @@
 package com.example.EmployeeManagement.controller;
 
 
+import com.example.EmployeeManagement.exception.ResourceNotFoundException;
 import com.example.EmployeeManagement.model.Country;
 import com.example.EmployeeManagement.model.Department;
 import com.example.EmployeeManagement.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -21,9 +23,17 @@ public class DepartmentController {
         return departmentRepository.findAll();
     }
 
-    @PostMapping("/countries")
+    @PostMapping("/departments")
     public Department createDepartment(@Valid @RequestBody Department department){
         return departmentRepository.save(department);
+    }
+
+    @GetMapping("/departments/{id}")
+    public ResponseEntity<Department> getDepartmentById(@PathVariable(value = "id") Long departmentId)
+        throws ResourceNotFoundException{
+        Department department = departmentRepository.findById(departmentId)
+                .orElseThrow(()-> new ResourceNotFoundException("The department with that id not found:" + departmentId));
+        return ResponseEntity.ok().body(department);
     }
 
 
