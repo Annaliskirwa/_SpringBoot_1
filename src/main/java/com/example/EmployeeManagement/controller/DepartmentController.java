c9f55748a0e85c3c7c8dfd9feaa7e6e0fdb14cba
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-05 23:37:19 +0300

    get all departments

--- src/main/java/com/example/EmployeeManagement/controller/DepartmentController.java
+++ src/main/java/com/example/EmployeeManagement/controller/DepartmentController.java
@@ -1,9 +1,8 @@
 package com.example.EmployeeManagement.controller;
 
 
-import com.example.EmployeeManagement.model.Country;
-import com.example.EmployeeManagement.repository.CountryRepository;
-import com.example.EmployeeManagement.repository.EmployeeRepository;
+import com.example.EmployeeManagement.model.Department;
+import com.example.EmployeeManagement.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +12,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class DepartmentController {
+    @Autowired
+    private DepartmentRepository departmentRepository;
 
+    @GetMapping("/departments")
+    public List<Department> getAllDepartments(){
+        return departmentRepository.findAll();
+    }
 
 }
