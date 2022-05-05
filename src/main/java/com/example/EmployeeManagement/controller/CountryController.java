968f2086626e9dbffcf8ccaa031c4169ceacc8c8
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-05 21:34:09 +0300

    Delete country error handler

--- src/main/java/com/example/EmployeeManagement/controller/CountryController.java
+++ src/main/java/com/example/EmployeeManagement/controller/CountryController.java
@@ -2,13 +2,16 @@ package com.example.EmployeeManagement.controller;
 
 import com.example.EmployeeManagement.exception.ResourceNotFoundException;
 import com.example.EmployeeManagement.model.Country;
+import com.example.EmployeeManagement.model.Employee;
 import com.example.EmployeeManagement.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -36,14 +39,30 @@ public class CountryController {
     public List<Country> findByCountryName(@PathVariable String countryName){
         return countryRepository.findByCountryName(countryName);
     }
-    /@DeleteMapping("/countries/{id}")
-    public String deleteCountryById(@PathVariable("id") Long countryId){
-        countryRepository.deleteById(countryId);
-        return "The country has been deleted";
+    @DeleteMapping("/countries/{id}")
+    public Map<String, Boolean> deleteCountryById(@PathVariable(value = "id") Long countryId)
+        throws ResourceNotFoundException{
+        Country country = countryRepository.findById(countryId)
+                .orElseThrow(()-> new ResourceNotFoundException("Country not found with this id:" + countryId));
+        countryRepository.delete(country);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("The country was deleted", Boolean.TRUE);
+        return response;
     }
+    //Ambigous methods throwing errors
+    /**
     @DeleteMapping("/countries/{countryName}")
     public String deleteCountryByName(@PathVariable String countryName){
         countryRepository.deleteByCountryName(countryName);
         return "The country has been deleted";
     }
+    */
+    //Delete a country with id without the error handler method
+
+    /**
+     *
+     *     public String deleteCountryById(@PathVariable("id") Long countryId){
+     *         countryRepository.deleteById(countryId);
+     *         return "The country has been deleted";
+     *     }*/
 }
