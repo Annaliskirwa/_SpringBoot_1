1b213a8456c9951d1a41d48d206086f034f40f10
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-05 10:19:30 +0300

    Generates the PK for department table

--- src/main/java/com/example/EmployeeManagement/model/Department.java
+++ src/main/java/com/example/EmployeeManagement/model/Department.java
@@ -1,9 +1,15 @@
 package com.example.EmployeeManagement.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "department")
+
 public class Department {
     private int departmentId;
     private String departmentName;
@@ -12,4 +18,18 @@ public class Department {
     public Department(){
 
     }
+    public Department(String departmentName){
+        this.departmentName = departmentName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
 }
