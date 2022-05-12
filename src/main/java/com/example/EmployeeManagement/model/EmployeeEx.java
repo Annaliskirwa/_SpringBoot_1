4c746eb28bec0130b4bdf5cf49d26bdb50d95c03
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-12 12:14:32 +0300

    hashcode designation

--- src/main/java/com/example/EmployeeManagement/model/EmployeeEx.java
+++ src/main/java/com/example/EmployeeManagement/model/EmployeeEx.java
@@ -39,4 +39,11 @@ public class EmployeeEx {
     public void setSalary(double salary) {
         this.salary = salary;
     }
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((designation == null)?0: designation.hashCode());
+    }
+
 }
