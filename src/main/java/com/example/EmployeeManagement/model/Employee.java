7991070220a8b8210588b68cc58d8228cd3f4b87
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-12 11:30:11 +0300

    adde the swagger dependancies

--- src/main/java/com/example/EmployeeManagement/model/Employee.java
+++ src/main/java/com/example/EmployeeManagement/model/Employee.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "employees", indexes = @Index(name = "fn_index",columnList = "firstName, lastName"))
+@Table(name = "employees", indexes = @Index(name = "fn_index",columnList = "firstName, lastName DESC"))
 public class Employee {
 
     private long id;
