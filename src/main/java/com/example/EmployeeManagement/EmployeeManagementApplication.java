8e07b14db02448a3c3fa42ff168bf55178721088
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-12 15:49:24 +0300

    changed other swagger properties

--- src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
@@ -1,9 +1,12 @@
 package com.example.EmployeeManagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Employees API", version = "1.0", description = "Employees endpoints info"))
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/EmployeeManagement/controller/EmployeeExController.java
+++ src/main/java/com/example/EmployeeManagement/controller/EmployeeExController.java
@@ -25,7 +25,7 @@ public class EmployeeExController {
                 break;
             }
         }
-        return deleteEmployee;
+        return null;
     }
 
     @PostMapping
