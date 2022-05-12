5d48c72251f099de084a6e963927649be2eea584
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-12 11:33:28 +0300

    created the employee extra model class

---
+++ src/main/java/com/example/EmployeeManagement/model/EmployeeEx.java
@@ -0,0 +1,8 @@
+package com.example.EmployeeManagement.model;
+
+public class EmployeeEx {
+    private String empId;
+    private String name;
+    private String designation;
+    private double salary;
+}
