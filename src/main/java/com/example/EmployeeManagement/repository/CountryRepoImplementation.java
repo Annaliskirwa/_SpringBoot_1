a4de7f04268b7a3ad778e903411be1b11f4c3370
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-19 15:33:24 +0300

    name parameter jdbc template to find country by name

--- src/main/java/com/example/EmployeeManagement/repository/CountryRepoImplementation.java
+++ src/main/java/com/example/EmployeeManagement/repository/CountryRepoImplementation.java
@@ -1,2 +1,44 @@
-package com.example.EmployeeManagement.repository;public class CountryRepoImplementation {
+package com.example.EmployeeManagement.repository;
+
+import com.example.EmployeeManagement.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public abstract class CountryRepoImplementation implements CountryRepo {
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Override
+    public int update(Country country) {
+        return namedParameterJdbcTemplate.update("update country set country_Name = :country_Name where country_Id = :country_Id",
+                new BeanPropertySqlParameterSource(country));
+    }
+
+    @Override
+    public List<Country> findByName(String countryName) {
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("countryName", countryName);
+
+        return namedParameterJdbcTemplate.query(
+                "select * from country where country_Name like :country_Name", mapSqlParameterSource,
+                ((rs, rowNum) -> new Country(
+                        rs.getLong("countryId"),
+                        rs.getString("countryName")
+                ))
+        );
+    }
+
+    @Override
+    public Optional<Country> findByCountryId(Long countryId) {
+        return Optional.empty();
+    }
+
+
 }
