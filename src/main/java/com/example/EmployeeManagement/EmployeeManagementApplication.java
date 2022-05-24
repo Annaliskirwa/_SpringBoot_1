56efc2b37e238ad287ff75d74c120f16bf65e9bf
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-24 20:44:44 +0300

    configured modelmapper

--- src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
@@ -2,12 +2,18 @@ package com.example.EmployeeManagement;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Employees API", version = "1.0", description = "Employees endpoints info"))
 public class EmployeeManagementApplication {
+	@Bean
+	public ModelMapper modelMapper(){
+		return  new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
