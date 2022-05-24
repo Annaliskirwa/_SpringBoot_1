f533ebadc8111991d0e2fca4ca82a7bdcdd2ca38
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-24 16:32:20 +0300

    implememted the post repo service methods

--- src/main/java/com/example/EmployeeManagement/service/PostServiceImpl.java
+++ src/main/java/com/example/EmployeeManagement/service/PostServiceImpl.java
@@ -1,4 +1,37 @@
 package com.example.EmployeeManagement.service;
 
-public class PostServiceImpl {
+import com.example.EmployeeManagement.model.Post;
+import com.example.EmployeeManagement.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+    private PostRepository postRepository;
+
+    @Override
+    public List<Post> getAllPosts() {
+        return null;
+    }
+
+    @Override
+    public Post createPost(Post post) {
+        return null;
+    }
+
+    @Override
+    public Post updatePost(long id, Post post) {
+        return null;
+    }
+
+    @Override
+    public void deletePost(long id) {
+
+    }
+
+    @Override
+    public Post getPostById(long id) {
+        return null;
+    }
 }
