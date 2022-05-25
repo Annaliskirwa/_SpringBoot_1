7cc99e16c963fadec61b01c3da88f7a1e31ddc4a
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-25 09:24:00 +0300

    get post by id controller

--- src/main/java/com/example/EmployeeManagement/controller/PostsController.java
+++ src/main/java/com/example/EmployeeManagement/controller/PostsController.java
@@ -1,11 +1,14 @@
 package com.example.EmployeeManagement.controller;
 
 import com.example.EmployeeManagement.dto.PostDto;
+import com.example.EmployeeManagement.exception.ResourceNotFoundException;
 import com.example.EmployeeManagement.model.Post;
 import com.example.EmployeeManagement.service.PostService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,14 @@ public class PostsController {
     @GetMapping
     public List<PostDto> getAllPosts(){
         return  postService.getAllPosts().stream().map(post -> modelMapper.map(post, PostDto.class)).collect(Collectors.toList());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id) throws ResourceNotFoundException {
+        Post post = postService.getPostById(id);
+
+        //Here I convert the entity to dto
+        PostDto postResponse = modelMapper.map(post, PostDto.class);
+
+        return ResponseEntity.ok().body(postResponse);
     }
 }
