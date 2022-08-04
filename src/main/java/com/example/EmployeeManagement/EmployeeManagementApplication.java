bc71647eabea1bf285b7bc32a66b21267c628cf8
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-05 00:08:54 +0300

    configured http security

--- src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
@@ -5,12 +5,15 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Employees API", version = "1.0", description = "Employees endpoints info"))
-public class EmployeeManagementApplication {
+public class EmployeeManagementApplication extends WebSecurityConfigurerAdapter {
 	@Bean
 	public ModelMapper modelMapper(){
 		return  new ModelMapper();
@@ -24,4 +27,10 @@ public class EmployeeManagementApplication {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
+
+//	@Override
+	//we have stopped the csrf to make post method work
+	protected void configure(HttpSecurity http) throws Exception{
+		http.cors().and().csrf().disable();
+	}
 }
